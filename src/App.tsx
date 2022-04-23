5fb486fde861ae01a19a5c55e4022f646d5adfd7
Angel David Hurtado Franco <angeldavidhf@gmail.com>
2022-04-23 15:24:29 -0500

    SCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import LoadingSpinner from '@molecules/LoadingSpinner';
 
@@ -10,7 +8,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <p>Hello Vite + React!</p>
                 <p>
                     <button type="button" onClick={() => setCount((count) => count + 1)}>
